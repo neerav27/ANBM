5cc2272db6741ccd8d9ef1b9e833c95ffc558914
anchao0207 <atrieu@knox.edu>
2022-05-16 14:05:17 -0500

    push to heroku

--- anbm/src/config.js
+++ anbm/src/config.js
@@ -4,5 +4,5 @@ const local = process.env.REACT_APP_BASE_URL_LOCAL;
 const heroku = process.env.REACT_APP_BASE_URL_HEROKU;
 export const axiosInstance = axios.create({
   //baseURL: "https://band-cs322.herokuapp.com/api"
-  baseURL: local,
+  baseURL: heroku,
 });
