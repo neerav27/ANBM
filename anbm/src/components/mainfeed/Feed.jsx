55298fa0e1adf0c36e18e2ffea15c58505d5cc19
anchao0207 <atrieu@knox.edu>
2022-05-16 14:02:45 -0500

    added logout button

--- anbm/src/components/mainfeed/Feed.jsx
+++ anbm/src/components/mainfeed/Feed.jsx
@@ -4,50 +4,47 @@ import Share from "../share/Share";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import { axiosInstance } from "../../config";
-import axios from "axios";
-import Darkmode from 'darkmode-js';
+import Darkmode from "darkmode-js";
 
 new Darkmode().showWidget();
 const options = {
-    bottom: '64px', // default: '32px'
-    right: 'unset', // default: '32px'
-    left: '32px', // default: 'unset'
-    time: '0.1s', // default: '0.3s'
-    mixColor: '#fff', // default: '#fff'
-    backgroundColor: '##FF5733',  // default: '#fff'
-    buttonColorDark: '#100f2c',  // default: '#100f2c'
-    buttonColorLight: '#FF5733', // default: '#fff'
-    saveInCookies: false, // default: true,
-    label: '🌓', // default: ''
-    autoMatchOsTheme: true // default: true
-  }
-  
-  
+  bottom: "64px", // default: '32px'
+  right: "unset", // default: '32px'
+  left: "32px", // default: 'unset'
+  time: "0.1s", // default: '0.3s'
+  mixColor: "#fff", // default: '#fff'
+  backgroundColor: "##FF5733", // default: '#fff'
+  buttonColorDark: "#100f2c", // default: '#100f2c'
+  buttonColorLight: "#FF5733", // default: '#fff'
+  saveInCookies: false, // default: true,
+  label: "🌓", // default: ''
+  autoMatchOsTheme: true, // default: true
+};
 
 export default function Feed({ username }) {
-    const [posts, setPosts] = useState([])
-    const { user } = useContext(AuthContext)
-    useEffect(() => {
-        const fetchPosts = async () => {
-            const res = username
-                ? await axiosInstance.get("/posts/profile/" + username)
-                : await axiosInstance.get("posts/timeline/" + user._id)
-                setPosts(
-                    res.data.sort((p1, p2) => {
-                      return new Date(p2.createdAt) - new Date(p1.createdAt);
-                    })
-                );
-        };
-        fetchPosts();
-    }, [username, user._id]);
-    return (
-        <div className="feed">
-            <div className="feedWrapper">
-                <Share />
-                {posts.map(p => (
-                    <Post key={p._id} post={p} />
-                ))}
-            </div>
-        </div>
-    )
+  const [posts, setPosts] = useState([]);
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = username
+        ? await axiosInstance.get("/posts/profile/" + username)
+        : await axiosInstance.get("posts/timeline/" + user._id);
+      setPosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    };
+    fetchPosts();
+  }, [username, user._id]);
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        {posts.map((p) => (
+          <Post key={p._id} post={p} />
+        ))}
+      </div>
+    </div>
+  );
 }
--- anbm/src/components/rightsidebar/Rightsidebar.jsx
+++ anbm/src/components/rightsidebar/Rightsidebar.jsx
@@ -1,4 +1,4 @@
-import "./rightsidebar.css"
+import "./rightsidebar.css";
 import { FeaturedPlayList } from "@material-ui/icons";
 import { Face } from "@material-ui/icons";
 import { MusicNote } from "@material-ui/icons";
@@ -8,160 +8,197 @@ import { Users } from "../../dummyData";
 import Suggest from "../suggest/Suggest";
 import Profile from "../../pages/profile/profile";
 import { useContext, useEffect } from "react";
-import {Add, Remove} from "@material-ui/icons";
+import { Add, Remove } from "@material-ui/icons";
 import { useState } from "react";
-import  {axios}  from "axios";
+import { axiosInstance, AxiosInstance } from "../../config";
 import { AuthContext } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
 
 export default function Rightsidebar({ user }) {
-    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-    const [friends, setFriends] = useState([]);
-    const {user:currentUser, dispatch} = useContext(AuthContext);
-    const [followed, setFollowed] = useState(
-        currentUser.followings.includes(user?.id)
-      );
+  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [friends, setFriends] = useState([]);
+  const { user: currentUser, dispatch } = useContext(AuthContext);
+  const [followed, setFollowed] = useState(
+    currentUser.followings.includes(user?.id)
+  );
 
-    useEffect(()=>{
-        setFollowed(currentUser.followings.includes(user?.id))
-        
-    },[currentUser, user?.id]);
+  useEffect(() => {
+    setFollowed(currentUser.followings.includes(user?.id));
+  }, [currentUser, user?.id]);
 
-    useEffect(()=>{
-        const getFriends = async () => {
-            try{
-                const friendList = await axios.get("/users/friends/" +user._id);
-                setFriends(friendList.data);
-            }catch(err){
-                console.log(err);
-            }
-        }; 
-        getFriends();
-     }, [user]);
-      
+  useEffect(() => {
+    const getFriends = async () => {
+      try {
+        const friendList = await axiosInstance.get(
+          "/users/friends/" + user._id
+        );
+        setFriends(friendList.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getFriends();
+  }, [user]);
 
-     const handleClick = async () => {
-        try {
-          if (followed) {
-            await axios.put(`/users/${user._id}/unfollow`, {
-              userId: currentUser._id,
-            });
-            dispatch({ type: "UNFOLLOW", payload: user._id });
-          } else {
-            await axios.put(`/users/${user._id}/follow`, {
-              userId: currentUser._id,
-            });
-            dispatch({ type: "FOLLOW", payload: user._id });
-          }
-            setFollowed(!followed);
-        } catch (err) {
-        }
-      };
-    const HomeRightbar = () => {
-        return (
-            <>
-                <ul className="rightsidebarList">
-                    <li className="rightsidebarListItem">
-                        <FeaturedPlayList className="rightsidebarIcon" />
-                        <span className="rightsidebarListItemText">Suggested Songs</span>
-                    </li>
-                    <li className="rightsidebarListItem">
-                        <Face className="rightsidebarIcon" />
-                        <span className="rightsidebarListItemText">Trending Artists</span>
-                    </li>
-                    <li className="rightsidebarListItem">
-                        <MusicNote className="rightsidebarIcon" />
-                        <span className="rightsidebarListItemText">Trending Songs</span>
-                    </li>
-                    <li className="rightsidebarListItem">
-                        <TrendingUp className="rightsidebarIcon" />
-                        <span className="rightsidebarListItemText">Trending Profiles</span>
-                    </li>
-                    <li className="rightsidebarListItem">
-                        <Whatshot className="rightsidebarIcon" />
-                        <span className="rightsidebarListItemText">Location Based</span>
-                    </li>
-                </ul>
-                <button className="rightsidebarButton">Show More</button>
-                <hr className="rightsidebarHr" />
-                {/* 
+  const handleClick = async () => {
+    try {
+      if (followed) {
+        await axiosInstance.put(`/users/${user._id}/unfollow`, {
+          userId: currentUser._id,
+        });
+        dispatch({ type: "UNFOLLOW", payload: user._id });
+      } else {
+        await axiosInstance.put(`/users/${user._id}/follow`, {
+          userId: currentUser._id,
+        });
+        dispatch({ type: "FOLLOW", payload: user._id });
+      }
+      setFollowed(!followed);
+    } catch (err) {}
+  };
+  const HomeRightbar = () => {
+    return (
+      <>
+        <ul className="rightsidebarList">
+          <li className="rightsidebarListItem">
+            <FeaturedPlayList className="rightsidebarIcon" />
+            <span className="rightsidebarListItemText">Suggested Songs</span>
+          </li>
+          <li className="rightsidebarListItem">
+            <Face className="rightsidebarIcon" />
+            <span className="rightsidebarListItemText">Trending Artists</span>
+          </li>
+          <li className="rightsidebarListItem">
+            <MusicNote className="rightsidebarIcon" />
+            <span className="rightsidebarListItemText">Trending Songs</span>
+          </li>
+          <li className="rightsidebarListItem">
+            <TrendingUp className="rightsidebarIcon" />
+            <span className="rightsidebarListItemText">Trending Profiles</span>
+          </li>
+          <li className="rightsidebarListItem">
+            <Whatshot className="rightsidebarIcon" />
+            <span className="rightsidebarListItemText">Location Based</span>
+          </li>
+        </ul>
+        <button className="rightsidebarButton">Show More</button>
+        <hr className="rightsidebarHr" />
+        {/* 
                 <ul className="rightsidebarFriendList">
                     {Users.map(u => (
                         <Suggest key={u.id} user={u} />
                     ))}
                 </ul> */}
-            </>
-        );
-    };
+      </>
+    );
+  };
 
-    const ProfileRightbar = () => {
-        return (
-            <>
-            {user.username !== currentUser.username && (
+  const ProfileRightbar = () => {
+    return (
+      <>
+        {user.username !== currentUser.username && (
           <button className="rightbarFollowButton" onClick={handleClick}>
             {followed ? "Unfollow" : "Follow"}
             {followed ? <Remove /> : <Add />}
           </button>
-            )}
-                <h4 className="rightsidebarTitle">Bio</h4>
-                <div className="rightsidebarInfo">
-                    <div className="rightsidebarInfoItem">
-                        <span className="rightsidebarInfoKey">Pronouns:</span>
-                        <span className="rightsidebarInfoValue"> {user.pronoun ? user.pronoun : "N/A"}</span>
-                    </div>
-                    <div className="rightsidebarInfoItem">
-                        <span className="rightsidebarInfoKey">Location:</span>
-                        <span className="rightsidebarInfoValue"> {user.location ? user.location : "N/A"} </span>
-                    </div>
-                    <div className="rightsidebarInfoItem">
-                        <span className="rightsidebarInfoKey">Age:</span>
-                        <span className="rightsidebarInfoValue"> {user.age ? user.age : "N/A"}</span>
-                    </div>
-                    <div className="rightsidebarInfoItem">
-                        <span className="rightsidebarInfoKey">Genre:</span>
-                        <span className="rightsidebarInfoValue"> {user.genre ? user.genre : "N/A"}</span>
-                    </div>
-                    <div className="rightsidebarInfoItem">
-                        <span className="rightsidebarInfoKey">Instruments:</span>
-                        <span className="rightsidebarInfoValue"> {user.instrument ? user.instrument : "N/A"}</span>
-                    </div>
-                </div>
-                <h4 className="rightsidebarTitle">Top Songs</h4>
-                <div className="rightsidebarFollowings">
-                    <div className="rightsidebarFollowing">
-                        <img src={`${PF}profile/photo1.jpg`} alt="" className="rightsidebarFollowingImg" />
-                        <span className="rightsidebarFollowingName"> Tipitipitiu</span>
-                    </div>
-                    <div className="rightsidebarFollowing">
-                        <img src={`${PF}profile/photo2.jpg`} alt="" className="rightsidebarFollowingImg" />
-                        <span className="rightsidebarFollowingName">John Cena</span>
-                    </div>
-                    <div className="rightsidebarFollowing">
-                        <img src={`${PF}profile/photo3.jpg`} alt="" className="rightsidebarFollowingImg" />
-                        <span className="rightsidebarFollowingName">John Cena</span>
-                    </div>
-                    <div className="rightsidebarFollowing">
-                        <img src={`${PF}profile/photo1.jpg`} alt="" className="rightsidebarFollowingImg" />
-                        <span className="rightsidebarFollowingName">John Cena</span>
-                    </div>
-                    <div className="rightsidebarFollowing">
-                        <img src={`${PF}profile/photo2.jpg`} alt="" className="rightsidebarFollowingImg" />
-                        <span className="rightsidebarFollowingName">John Cena</span>
-                    </div>
-                    <div className="rightsidebarFollowing">
-                        <img src={`${PF}profile/photo3.jpg`} alt="" className="rightsidebarFollowingImg" />
-                        <span className="rightsidebarFollowingName">John Cena</span>
-                    </div>
-                </div>
-            </>
-
-        )
-    }
-    return (
-        <div className="rightsidebar">
-            <div className="rightsidebarWrapper">
-                {user ? <ProfileRightbar /> : <HomeRightbar />}
-            </div>
+        )}
+        <h4 className="rightsidebarTitle">Bio</h4>
+        <div className="rightsidebarInfo">
+          <div className="rightsidebarInfoItem">
+            <span className="rightsidebarInfoKey">Pronouns:</span>
+            <span className="rightsidebarInfoValue">
+              {" "}
+              {user.pronoun ? user.pronoun : "N/A"}
+            </span>
+          </div>
+          <div className="rightsidebarInfoItem">
+            <span className="rightsidebarInfoKey">Location:</span>
+            <span className="rightsidebarInfoValue">
+              {" "}
+              {user.location ? user.location : "N/A"}{" "}
+            </span>
+          </div>
+          <div className="rightsidebarInfoItem">
+            <span className="rightsidebarInfoKey">Age:</span>
+            <span className="rightsidebarInfoValue">
+              {" "}
+              {user.age ? user.age : "N/A"}
+            </span>
+          </div>
+          <div className="rightsidebarInfoItem">
+            <span className="rightsidebarInfoKey">Genre:</span>
+            <span className="rightsidebarInfoValue">
+              {" "}
+              {user.genre ? user.genre : "N/A"}
+            </span>
+          </div>
+          <div className="rightsidebarInfoItem">
+            <span className="rightsidebarInfoKey">Instruments:</span>
+            <span className="rightsidebarInfoValue">
+              {" "}
+              {user.instrument ? user.instrument : "N/A"}
+            </span>
+          </div>
         </div>
+        <h4 className="rightsidebarTitle">Top Songs</h4>
+        <div className="rightsidebarFollowings">
+          <div className="rightsidebarFollowing">
+            <img
+              src={`${PF}profile/photo1.jpg`}
+              alt=""
+              className="rightsidebarFollowingImg"
+            />
+            <span className="rightsidebarFollowingName"> Tipitipitiu</span>
+          </div>
+          <div className="rightsidebarFollowing">
+            <img
+              src={`${PF}profile/photo2.jpg`}
+              alt=""
+              className="rightsidebarFollowingImg"
+            />
+            <span className="rightsidebarFollowingName">John Cena</span>
+          </div>
+          <div className="rightsidebarFollowing">
+            <img
+              src={`${PF}profile/photo3.jpg`}
+              alt=""
+              className="rightsidebarFollowingImg"
+            />
+            <span className="rightsidebarFollowingName">John Cena</span>
+          </div>
+          <div className="rightsidebarFollowing">
+            <img
+              src={`${PF}profile/photo1.jpg`}
+              alt=""
+              className="rightsidebarFollowingImg"
+            />
+            <span className="rightsidebarFollowingName">John Cena</span>
+          </div>
+          <div className="rightsidebarFollowing">
+            <img
+              src={`${PF}profile/photo2.jpg`}
+              alt=""
+              className="rightsidebarFollowingImg"
+            />
+            <span className="rightsidebarFollowingName">John Cena</span>
+          </div>
+          <div className="rightsidebarFollowing">
+            <img
+              src={`${PF}profile/photo3.jpg`}
+              alt=""
+              className="rightsidebarFollowingImg"
+            />
+            <span className="rightsidebarFollowingName">John Cena</span>
+          </div>
+        </div>
+      </>
     );
+  };
+  return (
+    <div className="rightsidebar">
+      <div className="rightsidebarWrapper">
+        {user ? <ProfileRightbar /> : <HomeRightbar />}
+      </div>
+    </div>
+  );
 }
