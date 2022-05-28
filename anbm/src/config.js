60eba9d110da9cc47b6af0af206862a420a216a6
anchao0207 <atrieu@knox.edu>
2022-05-28 15:50:50 -0500

    return

--- anbm/src/config.js
+++ anbm/src/config.js
@@ -3,5 +3,5 @@ import axios from "axios";
 const local = process.env.REACT_APP_BASE_URL_LOCAL;
 const heroku = process.env.REACT_APP_BASE_URL_HEROKU;
 export const axiosInstance = axios.create({
-  baseURL: heroku,
+  baseURL: local,
 });
